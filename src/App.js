23dce0fdb95e632002ea7f476a0764585eb1af87
MikeyCouture <michelcouture42@gmail.com>
2021-04-22 20:20:40 -0400

    Initial set up pt 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
   return (
     <ChatEngine
       height="100vh"
-      projectID="
-f5cab63c-d753-4216-9833-4fe063d3cd4a"
+      projectID=""
       userName="Mikey"
-      userSecret="09870987"
+      userSecret=""
+      renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps} />}
     />
   );
 };
